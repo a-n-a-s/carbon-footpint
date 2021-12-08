76324d0daba5be13f4e5d8dfa05fffcd66b73af1
a-n-a-s <mirzaanas815@.com>
2021-12-08 21:36:48 +0530

    o

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Main from "./Components/Main";
 import Footer from "./Components/footer";
+import About from "./Components/About";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <Main />
+      <About />
       <Footer />
     </>
   );
---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-4/5 mx-auto my-10">
+      <span>About the Page</span>
+      <p>
+          ..
+      </p>
+    </div>
+  );
+};
+
+export default About;
